59debcf05f53ff6525061103ffbf1c8e75b19f15
Vincent Vega <vi.fu.ve.ri.th@gmail.com>
2018-04-05 13:28:17 +0300

    try to commit from intellijIdea

--- src/test/Test.java
+++ src/test/Test.java
@@ -4,6 +4,14 @@ public class Test {
 
     public static void main(String[] args) {
         System.out.println("test!");
+        System.out.println("test!");
+        System.out.println("test!");
+        System.out.println("test!");
+        System.out.println("test!");
+        System.out.println("test!");
+        System.out.println("test!");
+
+
     }
 
 }
