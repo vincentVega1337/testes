fa3a41ce9c53b659ac189b428bc54d649e47dae7
Vincent Vega <vi.fu.ve.ri.th@gmail.com>
2018-04-06 13:29:47 +0300

    add trees

--- src/com/skylite/test/binaryTrees/BinaryTree.java
+++ src/com/skylite/test/binaryTrees/BinaryTree.java
@@ -4,4 +4,47 @@ public class BinaryTree {
 
 
 
+    private class Node implements Comparable<Node>{
+        private Node left;
+        private Node right;
+        private int key;
+        private String value;
+
+        @Override
+        public int compareTo(Node o) {
+            return o.getKey() - this.key;
+        }
+
+        public Node getLeft() {
+            return left;
+        }
+
+        public void setLeft(Node left) {
+            this.left = left;
+        }
+
+        public Node getRight() {
+            return right;
+        }
+
+        public void setRight(Node right) {
+            this.right = right;
+        }
+
+        public int getKey() {
+            return key;
+        }
+
+        public void setKey(int key) {
+            this.key = key;
+        }
+
+        public String getValue() {
+            return value;
+        }
+
+        public void setValue(String value) {
+            this.value = value;
+        }
+    }
 }
